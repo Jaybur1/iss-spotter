77ee4ee03603a6a23cb78c032b976a76df29463d
vagrant <jayburbyga@gmail.com>
2020-03-12 23:21:55 +0000

    added timezone format

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { nextISSTimesForMyLocation } = require("./iss");
+const { zonedTimeToUtc, utcToZonedTime, format } = require("date-fns-tz");
 
 //@passTimes - array of objects {duration: number, risetime: timestamp}
 nextISSTimesForMyLocation((err, passTimes) => {
-  if(err) return err;
+  if (err) return err;
 
   passTimes.forEach(pass => {
-    const time = new Date(pass.risetime * 1000)
-    
-    console.log(`Next pass at ${time.toString()}for ${pass.duration} seconds!`)
-  })
+    const time = new Date(pass.risetime * 1000);
+    // const date = utcToZonedTime(time, "America/Toronto");
+    const timeZone = "America/Toronto";
+    const zonedDate = utcToZonedTime(time, timeZone);
+    const pattern = "cccccc pp zzzz"//".m.yyy HH:mm:ss.SSS [GMT]Z (z)";
+    const output = format(zonedDate, pattern, { timeZone });
+    console.log(`Next pass at ${output} for ${pass.duration} seconds!`);
+  });
 });
-// -14400
